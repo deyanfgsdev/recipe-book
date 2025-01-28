4e8f1786ef9dab9d369a8ed714c4473b3a9705c0
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-01-28 14:51:12 +0000

    feat: add home page route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from '@/App';
+
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { App } from '@/App';
+import { Home } from '@/pages/Home';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
