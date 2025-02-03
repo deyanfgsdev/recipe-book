c66286f92b55af16a9f88421fa6b710214f8f57d
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-03 12:27:23 +0000

    feat: add Spoonacular API constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const MOBILE_BREAKPOINT = 992;
-
 export const MOBILE_HEADER_IMAGE =
   'https://i.ibb.co/npZ5DZZ/mobile-header-bg.jpg';
 export const DESKTOP_HEADER_IMAGE =
   'https://i.ibb.co/GSnYnBH/desktop-header-bg.jpg';
+export const SPOONACULAR_API_PREFIX = 'https://api.spoonacular.com';
+export const SPOONACULAR_API_KEY = import.meta.env.VITE_SPOONACULAR_API_KEY;
