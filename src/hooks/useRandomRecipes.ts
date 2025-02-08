bb122b8f20927c3a06ac80ef0741fc0ff4d18909
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-08 18:47:10 +0000

    refactor: rename Recipes to RandomRecipes type

--- src/hooks/useRandomRecipes.ts
+++ src/hooks/useRandomRecipes.ts
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react';
 
 import { getRandomRecipes } from '@/services/recipes';
 
-import type { Recipes } from '@/pages/Home.types';
+import type { RandomRecipes } from '@/pages/Home.types';
 
 export const useRandomRecipes = () => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [randomRecipes, setRandomRecipes] = useState<Recipes>(null);
+  const [randomRecipes, setRandomRecipes] = useState<RandomRecipes>(null);
 
   useEffect(() => {
     setLoading(true);
