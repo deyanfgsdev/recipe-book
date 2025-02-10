ce4167abaa1c683b7933f1fa1bcb0cc3525097a1
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-10 14:39:36 +0000

    fix: remove RandomRecipes type and add Recipes type

--- src/pages/Home.types.ts
+++ src/pages/Home.types.ts
@@ -1,7 +1,3 @@
 import type { MappedRecipe as Recipe } from '@/services/recipes.types';
 
-export type RandomRecipes = null | Recipe[];
-export interface SearchRecipes {
-  searchError: string | null;
-  recipes: null | Recipe[];
-}
+export type Recipes = null | Recipe[];
