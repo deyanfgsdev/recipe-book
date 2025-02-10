a3f48a7d653746e02a0466ec1d88653688e33ab0
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-10 16:12:26 +0000

    refactor: update randomRecipes state type

--- src/hooks/useRandomRecipes.ts
+++ src/hooks/useRandomRecipes.ts
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react';
 
 import { getRandomRecipes } from '@/services/recipes';
 
-import type { Recipes } from '@/pages/Home.types';
+import type { MappedRecipe as Recipe } from '@/services/recipes.types';
 
 export const useRandomRecipes = () => {
-  const [randomRecipes, setRandomRecipes] = useState<Recipes>(null);
+  const [randomRecipes, setRandomRecipes] = useState<Recipe[]>([]);
 
   useEffect(() => {
     getRandomRecipes()
