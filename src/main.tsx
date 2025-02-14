5bbd90d11fd0d15d7a05885c940837971e49ab7f
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-14 12:23:09 +0000

    feat: create favourites page with routing

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,14 @@ import '@fontsource/roboto/700.css';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { App } from '@/App';
 import { Home } from '@/pages/Home';
+import { Favourites } from '@/pages/Favourites';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
       </Route>
     </Routes>
   </BrowserRouter>
---
+++ src/pages/Favourites.tsx
@@ -0,0 +1,3 @@
+export const Favourites = () => {
+  return <section className="favourites-content">Favourites Recipes</section>;
+};
