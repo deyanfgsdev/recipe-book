775f32ff212fb2363b8c84289a7fbedabdd1d564
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-14 12:15:19 +0000

    feat: wrap app routes with FavouritesRecipesProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router';
 
 import { Header } from '@/components/Header/Header';
+import { FavouritesRecipesProvider } from '@/context/FavouritesRecipesProvider';
 
 export const App = () => {
   return (
     <>
       <Header />
       <main className="main bg-light-beige pt-[68px] pb-4">
-        <Outlet />
+        <FavouritesRecipesProvider>
+          <Outlet />
+        </FavouritesRecipesProvider>
       </main>
     </>
   );
