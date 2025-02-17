a0bff55b598cbfa6a83214ffa5975ece8388cf41
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-17 15:29:42 +0000

    feat: create Spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="spinner flex items-center justify-center">
+      <div className="border-light-green h-10 w-10 animate-spin rounded-full border-4 border-t-transparent" />
+    </div>
+  );
+};
