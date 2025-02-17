ac70033347f53e07f3128758fe26b47e95795433
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-17 13:28:43 +0000

    feat: render a list of favourite recipes

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,3 +1,43 @@
+import { useFavouritesRecipes } from '@/hooks/useFavouritesRecipes';
+
+import { RecipeCard } from '@/components/RecipeCard/RecipeCard';
+
+import { checkIfRecipeIsFavourite } from '@/utils/favourites';
+
 export const Favourites = () => {
-  return <section className="favourites-content">Favourites Recipes</section>;
+  const { favouritesRecipes } = useFavouritesRecipes();
+
+  const recipeListClassName =
+    'favourites-recipes-list grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6';
+
+  return (
+    <div className="favourites-page-content p-4">
+      <h1 className="favourites-page-content__title text-bold-grey text-center text-[32px] font-bold min-[768px]:text-[40px]">
+        Favourites Recipes
+      </h1>
+      <section className="favourites-page-content__recipes">
+        {!favouritesRecipes.length ? (
+          <p>No favourite recipes</p>
+        ) : (
+          <ul className={recipeListClassName}>
+            {favouritesRecipes.map((recipe) => {
+              const { recipeId } = recipe;
+              const isFavouriteRecipe = checkIfRecipeIsFavourite(
+                recipeId,
+                favouritesRecipes
+              );
+              return (
+                <RecipeCard
+                  key={recipeId}
+                  recipe={recipe}
+                  variant="favourite"
+                  isFavouriteRecipe={isFavouriteRecipe}
+                />
+              );
+            })}
+          </ul>
+        )}
+      </section>
+    </div>
+  );
 };
