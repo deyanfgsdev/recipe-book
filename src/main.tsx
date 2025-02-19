e975b97152e6aa1d0ffee62e4b3e6cc0c8c7df92
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-19 15:54:39 +0000

    feat: setup TanStack Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,29 @@
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 import './index.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router';
 import { App } from '@/App';
 import { HomePage } from '@/pages/HomePage';
 import { FavouritesPage } from '@/pages/FavouritesPage';
 import { RecipePage } from '@/pages/RecipePage';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<HomePage />} />
-        <Route path="favourites" element={<FavouritesPage />} />
-        <Route path="recipe/:sourceUrl" element={<RecipePage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="favourites" element={<FavouritesPage />} />
+          <Route path="recipe/:sourceUrl" element={<RecipePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
