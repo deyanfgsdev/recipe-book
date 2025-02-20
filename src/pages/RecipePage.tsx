8c9fa0d9b64ad90c374cd8c5e525656a30787754
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-20 12:55:22 +0000

    feat: implement recipe information query

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,7 +1,19 @@
 import { useParams } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import { getRecipeInformation } from '@/services/recipes';
+
+const getRecipeIdFromSourceUrl = (sourceUrl: string) =>
+  Number(sourceUrl.split('-').pop());
 
 export const RecipePage = () => {
   const { sourceUrl } = useParams();
+  const recipeId = sourceUrl
+    ? getRecipeIdFromSourceUrl(sourceUrl)
+    : Number(sourceUrl);
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['recipe', recipeId],
+    queryFn: async () => await getRecipeInformation(recipeId),
+  });
 
   return (
     <div>
