f792f67fb667a45c88af949e9f09cd8b440af88d
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-26 12:58:04 +0000

    feat: add recipe categories constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,57 @@ export const SPOONACULAR_API_KEY = import.meta.env.VITE_SPOONACULAR_API_KEY;
 export const SPOONACULAR_API_INGREDIENT_IMAGE_PREFIX =
   'https://img.spoonacular.com/ingredients_';
 export const SPOONACULAR_API_INGREDIENT_IMAGE_SIZE = '500x500';
+export const RECIPE_CATEGORIES = [
+  {
+    name: 'Main Course',
+    imageUrl: 'https://i.ibb.co/HT7YzHJ6/main-course.png',
+  },
+  {
+    name: 'Side Dish',
+    imageUrl: 'https://i.ibb.co/PRp5kKG/side-dish.png',
+  },
+  {
+    name: 'Dessert',
+    imageUrl: 'https://i.ibb.co/nsN57yVt/dessert.png',
+  },
+  {
+    name: 'Appetizer',
+    imageUrl: 'https://i.ibb.co/XZ21mfg7/appetizer.png',
+  },
+  {
+    name: 'Salad',
+    imageUrl: 'https://i.ibb.co/0jTXQMDj/salad.png',
+  },
+  {
+    name: 'Bread',
+    imageUrl: 'https://i.ibb.co/CKMPWbC1/bread.png',
+  },
+  {
+    name: 'Breakfast',
+    imageUrl: 'https://i.ibb.co/QjbRXNcs/breakfast.png',
+  },
+  {
+    name: 'Soup',
+    imageUrl: 'https://i.ibb.co/QjPK5W1B/soup.png',
+  },
+  {
+    name: 'Sauce',
+    imageUrl: 'https://i.ibb.co/gcRWHF2/sauce.png',
+  },
+  {
+    name: 'Marinade',
+    imageUrl: 'https://i.ibb.co/qYzHTXfZ/marinade.png',
+  },
+  {
+    name: 'Finger Food',
+    imageUrl: 'https://i.ibb.co/C3WL8CJn/fingerfood.png',
+  },
+  {
+    name: 'Snack',
+    imageUrl: 'https://i.ibb.co/ycmkGptz/snack.png',
+  },
+  {
+    name: 'Drink',
+    imageUrl: 'https://i.ibb.co/mC61mHVW/drink.png',
+  },
+];
