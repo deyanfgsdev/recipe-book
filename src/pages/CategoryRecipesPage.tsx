c5fb77e9c60675d94c99cb6d06d69f84e2b1e2d5
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-02-27 13:39:23 +0000

    feat: implement category recipes query

--- src/pages/CategoryRecipesPage.tsx
+++ src/pages/CategoryRecipesPage.tsx
@@ -1,11 +1,31 @@
 import { useParams } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import { getRecipesByCategory } from '@/services/recipes';
 
 export const CategoryRecipesPage = () => {
-  const { categoryName } = useParams();
+  const { categoryName: category } = useParams();
+  const {
+    isLoading,
+    isError,
+    data: recipes,
+  } = useQuery({
+    queryKey: ['categoryRecipes', category],
+    queryFn: async () => {
+      try {
+        if (!category) throw new Error('Category is required');
+
+        return await getRecipesByCategory(category);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        }
+      }
+    },
+  });
 
   return (
-    <div>
-      <h1>Category: {categoryName}</h1>
+    <div className="category-recipes-page-content p-4">
+      <h1>Category: {category}</h1>
     </div>
   );
 };
