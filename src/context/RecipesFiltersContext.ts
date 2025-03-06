ebc67801227e6d2bff3fe988acd86d57a6cddb40
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-03-06 15:08:47 +0000

    feat: add update methods to RecipesFiltersContext

--- src/context/RecipesFiltersContext.ts
+++ src/context/RecipesFiltersContext.ts
@@ -3,6 +3,8 @@ import { createContext } from 'react';
 interface RecipesFiltersContextType {
   readyInMaxMinutes: string;
   dietType: string;
+  updateReadyInMaxMinutes: (newReadyInMaxMinutes: string) => void;
+  updateDietType: (newDietType: string) => void;
 }
 
 export const RecipesFiltersContext = createContext<
