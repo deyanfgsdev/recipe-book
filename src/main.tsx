94d529c0308d73b25ec5d411a4d5ffc32eb74069
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-03-06 16:14:03 +0000

    feat: wrap HomePage component with RecipeFiltersProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { RecipeFiltersProvider } from '@/context/RecipeFiltersProvider';
 
 import './index.css';
 import '@fontsource/roboto/400.css';
@@ -21,7 +22,14 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
+          <Route
+            index
+            element={
+              <RecipeFiltersProvider>
+                <HomePage />
+              </RecipeFiltersProvider>
+            }
+          />
           <Route path="favourites" element={<FavouritesPage />} />
           <Route path="recipe/:sourceUrl" element={<RecipePage />} />
           <Route path="categories" element={<CategoriesPage />} />
