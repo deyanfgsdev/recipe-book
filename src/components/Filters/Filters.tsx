5b6e7bc6903cccbb2991113557e28808176823be
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-03-07 11:15:43 +0000

    feat: implement filter change handlers

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,13 +1,33 @@
 import { useId } from 'react';
+import { useRecipeFilters } from '@/hooks/useRecipeFilters';
 
 export const Filters = () => {
   const readyInMaxMinutesId = useId();
   const dietTypeId = useId();
+  const { filters, updateReadyInMaxMinutes, updateDietType } =
+    useRecipeFilters();
+
+  const handleChangeReadyInMaxMinutes = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value: newReadyInMaxMinutes } = event.target;
+    updateReadyInMaxMinutes(newReadyInMaxMinutes);
+  };
+
+  const handleChangeDietType = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { value: newDietType } = event.target;
+    updateDietType(newDietType);
+  };
 
   return (
     <div className="recipe-filters md:flex md:items-center md:justify-end md:gap-4">
-      <div className="recipe-filters__ready-in-minutes flex items-center gap-2">
-        <label htmlFor={readyInMaxMinutesId} className="md:text-lg">
+      <div className="recipe-filters-ready-in-minutes flex items-center gap-2">
+        <label
+          htmlFor={readyInMaxMinutesId}
+          className="recipe-filters-ready-in-minutes__label md:text-lg"
+        >
           Ready in minutes (max.):
         </label>
         <input
@@ -15,14 +35,27 @@ export const Filters = () => {
           id={readyInMaxMinutesId}
           min="0"
           max="120"
-          className="accent-bold-green"
+          className="recipe-filters-ready-in-minutes__input accent-bold-green"
+          value={filters.readyInMaxMinutes}
+          onChange={handleChangeReadyInMaxMinutes}
         />
+        <span className="recipe-filters-ready-in-minutes__info">
+          {filters.readyInMaxMinutes}
+        </span>
       </div>
-      <div className="recipe-filters__diet-type mt-4 flex items-center gap-2 md:mt-0">
-        <label htmlFor={dietTypeId} className="md:text-lg">
+      <div className="recipe-filters-diet-type mt-4 flex items-center gap-2 md:mt-0">
+        <label
+          htmlFor={dietTypeId}
+          className="recipe-filters-diet-type__label md:text-lg"
+        >
           Diet Type:
         </label>
-        <select id={dietTypeId} className="md:text-lg">
+        <select
+          id={dietTypeId}
+          className="recipe-filters-diet-type__select md:text-lg"
+          value={filters.dietType}
+          onChange={handleChangeDietType}
+        >
           <option value="all">All</option>
           <option value="vegetarian">Vegetarian</option>
           <option value="vegan">Vegan</option>
