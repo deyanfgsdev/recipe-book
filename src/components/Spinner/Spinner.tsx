5e441e57066cc235aa86e347b1295fc57a22e92a
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2025-03-12 15:45:15 +0000

    style: add margin top in spinner layout

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 export const Spinner = () => {
   return (
-    <div className="spinner flex items-center justify-center">
+    <div className="spinner mt-6 flex items-center justify-center">
       <div className="border-light-green h-10 w-10 animate-spin rounded-full border-4 border-t-transparent" />
     </div>
   );
